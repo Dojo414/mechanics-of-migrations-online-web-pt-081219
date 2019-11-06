6c47e1b5b9ee683c4b50099def15ad8ed9e6a250
Donovan Jones <donovanljones94@gmail.com>
2019-11-06 21:46:04 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,4 +1,4 @@
-class AssFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
   def change 
     add_column :artist, :favorite_food, :string 
   end
